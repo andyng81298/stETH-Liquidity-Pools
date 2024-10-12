0bd9df47eeb31c957fc7111e58b92c18c866e0ea
Broken Blade <140306885+andyng81298@users.noreply.github.com>
2024-10-12 14:51:21 +0700

    Create stETH Collateral on Polygon.sql

---
+++ stETH Collateral on Polygon.sql
@@ -0,0 +1,34 @@
+with lendings_list as (
+ select
+      project
+    from
+      dune.lido.result_wsteth_in_lending_pools
+    where
+      time = (
+        select
+          max(time)
+        from
+          dune.lido.result_wsteth_in_lending_pools
+      )
+      and amount >= 100
+      and blockchain = 'polygon'
+
+) 
+, collateral_by_project as (
+select  cast(time as timestamp) as time, 	
+        project, 
+        sum(amount) as amount
+        
+from dune.lido.result_wsteth_in_lending_pools
+
+where  date_trunc('month', time) >= date_trunc('month',now()) - interval '3' month
+  and blockchain = 'polygon'
+  and project in (select project from lendings_list)
+group by 1,2
+)
+
+
+
+select collateral_by_project.*
+from collateral_by_project
+order by collateral_by_project.time desc, amount desc
